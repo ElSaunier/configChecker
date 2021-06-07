1cd0c945cf421990029c96c021ae37fcf857b585
SAUNIER Loic <loic.saunier@docaposte.fr>
2021-06-07 15:19:16 +0200

    Test d'intégration de promconfig

--- verification.go
+++ verification.go
@@ -1,5 +1,5 @@
 import (
-	"promconfig-main/promconfig"
+	pconfig "promconfig-main/promconfig"
 )
 
 type ConfigFile {
@@ -10,7 +10,7 @@ type ConfigFile {
 func (config *ConfigFile) Validate() bool{
 	config.Errors = make(map[string]string)
 
-	
+
 	promconfig.TestGoldenData()
 
 	return len(config.Errors) == 0
