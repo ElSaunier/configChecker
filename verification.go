230eb0ade5ecb8508071d531494e1a535731cdc7
SAUNIER Loic <loic.saunier@docaposte.fr>
2021-06-07 14:43:41 +0200

    New verification Signed-off-by: SAUNIER Loic <loic.saunier@docaposte.fr>

---
+++ verification.go
@@ -0,0 +1,16 @@
+import (
+	"promconfig-main/promconfig"
+)
+
+type ConfigFile {
+	Content string
+	Errors map[string]string
+}
+
+func (config *ConfigFile) Validate() bool{
+	config.Errors = make(map[string]string)
+
+	promconfig.TestGoldenData()
+
+	return len(config.Errors) == 0
+}
