8a3fe2032e012bd732d329c1de1efc0a6aa5f189
SAUNIER Loic <loic.saunier@docaposte.fr>
2021-06-08 09:16:58 +0200

    Suppression du package

--- index.go
+++ index.go
@@ -5,8 +5,10 @@ import (
 	"net/http"
 	"log"
 	"configChecker"
+	"fmt"
 
 	"github.com/bmizerany/pat"
+	//pconfig "github.com/percona/promconfig"
 )
 
 type Home struct {
@@ -18,6 +20,11 @@ type Verif struct {
 	Title string
 	Description string
 	Result string
+}
+
+type ConfigFile struct{
+	Content string
+	Errors map[string]string
 }
 
 func createTemplate(w http.ResponseWriter, filename string, data interface{}) {
@@ -45,6 +52,50 @@ func sendHandler(w http.ResponseWriter, r *http.Request){
 func verifHandler(w http.ResponseWriter, r *http.Request){
 	createTemplate(w, "templates/home.html", nil)
 }
+
+func (config *ConfigFile) Validate() bool{
+	config.Errors = make(map[string]string)
+	b := []byte(config.Content)
+
+	fmt.Println("[+] Checking ...")
+
+	fmt.Println(b)
+	/*
+	matches, err := filepath.Glob("testdata/*.yml")
+	require.NoError(t, err)
+	require.NotEmpty(t, matches)
+
+	for _, yf := range matches {
+		b, err := ioutil.ReadFile(yf) //=> returns bytes
+		require.NoError(t, err) //=> Similar to err != nil
+
+		var cfg Config
+		err = yaml.Unmarshal(b, &cfg)
+		require.NoError(t, err)
+		actualB, err := json.MarshalIndent(cfg, "", "  ")
+		require.NoError(t, err)
+		actualB = append(actualB, '\n')
+
+		jf := strings.TrimSuffix(yf, filepath.Ext(yf)) + ".json"
+
+		if *goldenF {
+			err = ioutil.WriteFile(jf, actualB, 0644)
+			require.NoError(t, err)
+		}
+
+		expectedB, err := ioutil.ReadFile(jf)
+		require.NoError(t, err)
+
+		expectedS := string(expectedB)
+		actualS := string(actualB)
+		assert.Equal(t, expectedS, actualS)
+	}
+	*/
+	fmt.Println("[-] Checking finished")
+
+	return len(config.Errors) == 0
+}
+
 
 func main() {
 
