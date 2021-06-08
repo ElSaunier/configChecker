a6fdde063a44a4109177f6957a9620ac0f96a33f
SAUNIER Loic <loic.saunier@docaposte.fr>
2021-06-08 09:53:58 +0200

    Début d'adaptation

--- index.go
+++ index.go
@@ -59,28 +59,17 @@ func (config *ConfigFile) Validate() bool{
 	fmt.Println("[+] Checking ...")
 
 	fmt.Println(b)
-	/*
-	matches, err := filepath.Glob("testdata/*.yml")
-	require.NoError(t, err)
-	require.NotEmpty(t, matches)
 
 	for _, yf := range matches {
 		b, err := ioutil.ReadFile(yf) //=> returns bytes
 		require.NoError(t, err) //=> Similar to err != nil
 
 		var cfg Config
-		err = yaml.Unmarshal(b, &cfg)
+		err = yaml.Unmarshal(b, &cfg) // Attribue le yaml à l'identité correspondante
 		require.NoError(t, err)
 		actualB, err := json.MarshalIndent(cfg, "", "  ")
 		require.NoError(t, err)
 		actualB = append(actualB, '\n')
-
-		jf := strings.TrimSuffix(yf, filepath.Ext(yf)) + ".json"
-
-		if *goldenF {
-			err = ioutil.WriteFile(jf, actualB, 0644)
-			require.NoError(t, err)
-		}
 
 		expectedB, err := ioutil.ReadFile(jf)
 		require.NoError(t, err)
@@ -89,7 +78,7 @@ func (config *ConfigFile) Validate() bool{
 		actualS := string(actualB)
 		assert.Equal(t, expectedS, actualS)
 	}
-	*/
+	
 	fmt.Println("[-] Checking finished")
 
 	return len(config.Errors) == 0
