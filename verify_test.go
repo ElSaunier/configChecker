83925e369f63b6166b91c6545c7540515ee3d859
SAUNIER Loic <loic.saunier@docaposte.fr>
2021-06-09 16:48:24 +0200

    Ajout des tests

---
+++ verify_test.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"testing"
+	"os"
+	"os/exec"
+)
+
+func TestValidatePromtool(t *testing.T) {
+	file,err := os.open("testdata/test.yml")
+	if err != nil {
+		t.Errorf("Cannot open File : %s\n",err)
+	}
+	
+	cmd := exec.Command("promtool","check","config",file.Name())
+   	out, err = cmd.CombinedOutput()
+    if err != nil {
+    	t.Errorf("cmd.Run() failed with %s\n", err)
+   	}
+}
+
+func TestValidateAlertManager(t *testing.T) {
+	file,err := os.open("testdata/testam.yml")
+	if err != nil {
+		t.Errorf("Cannot open File : %s\n",err)
+	}
+
+	cmd := exec.Command("amtool","check-config",file.Name())
+   	out, err = cmd.CombinedOutput()
+    if err != nil {
+    	t.Errorf("cmd.Run() failed with %s\n", err)
+   	}    
+}
+
